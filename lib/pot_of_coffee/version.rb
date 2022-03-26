8f1d1a58588e006dc62e5c9ffe886eaaed3e67f0
David Shaffer <dave.shaffer@gmail.com>
2022-03-26 02:05:41 -0400

    Update version

--- lib/pot_of_coffee/version.rb
+++ lib/pot_of_coffee/version.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 module PotOfCoffee
-  VERSION = "0.4.0"
+  MAJOR = 0
+  MINOR = 4
+  PATCH = 1
+  RELEASE = nil
+
+  VERSION = [MAJOR, MINOR, PATCH, RELEASE].compact.join(".")
 end
